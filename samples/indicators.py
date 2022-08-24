eb48ea7954afc1f6719ca7ad22fbc48011065c4c
Giovanni L <callmegiorgio@hotmail.com>
2022-08-24 10:44:09 -0300

    updated samples

--- samples/indicators.py
+++ samples/indicators.py
@@ -10,9 +10,14 @@ Options:
 
 import docopt
 import icvm
+import pandas as pd
 import time
 import sys
 import zipfile
+
+def print_indicator(i):
+    print(f'\n{i.__class__.__name__}:')
+    print(pd.DataFrame([i]).transpose()[0].to_string())
 
 def main():
     args = docopt.docopt(__doc__)
@@ -26,16 +31,11 @@ def main():
             indebtedness, profitability, efficiency = res.indicators
 
             print('----------------------------')
-            print('Company:', res.dfpitr.company_name)
+            print(f'Company: {res.dfpitr.company_name} ({res.balance_type})')
             
-            print('\nIndebtedness:')
-            print(indebtedness.series())
-            
-            print('\nEfficiency:')
-            print(efficiency.series())
-            
-            print('\nProfitability:')
-            print(profitability.series())
+            print_indicator(indebtedness)
+            print_indicator(efficiency)
+            print_indicator(profitability)
 
             time.sleep(2)
 
--- samples/valuation.py
+++ samples/valuation.py
@@ -8,8 +8,10 @@ Options:
   --help  Show this screen.
 """
 
+import cvm
 import docopt
 import icvm
+import pandas as pd
 import sys
 import zipfile
 
@@ -19,16 +21,15 @@ def main():
     filepath = args['<dfpitr>']
 
     with zipfile.ZipFile(filepath) as file:
-        reader = icvm.reader(file, [icvm.YfinanceValuation])
+        reader = icvm.reader(file, [icvm.YfinanceValuation], flag=cvm.csvio.BalanceFlag.CONSOLIDATED)
 
         for res in reader:
-            valuation = res.indicators[0]
-
-            print('------------------')
-            print('Company:', res.dfpitr.company_name)
-            
-            print('\nValuation:')
-            print(valuation.series())
+            for valuation in res.indicators:
+                print('------------------')
+                print('Company:', res.dfpitr.company_name)
+                
+                print('\nValuation:')
+                print(pd.DataFrame([valuation]).transpose()[0].to_string())
 
     return 0
 
